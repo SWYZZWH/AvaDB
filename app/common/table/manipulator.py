bb4e5a30a1be042022143942b17eeeaaea54a596
zwh <843532380@qq.com>
2023-11-14 01:10:46 -0800

    implement group by operation on single column. support aggregator functions: max, min, sum, count, avg

--- app/common/table/manipulator.py
+++ app/common/table/manipulator.py
@@ -1,5 +1,6 @@
 import math
 from collections import Counter
+from enum import Enum
 
 import config
 import constant
@@ -19,18 +20,81 @@ import random
 
 class SortOption:
 
-    def __init__(self, column: str, is_asc: bool, ways: int = config.max_chunk_size):
+    def __init__(self, column: str, is_asc: bool = True, ways: int = config.max_chunk_size):
         self.column = column
         self.is_asc = is_asc
         self.ways = ways  # how many ways are used in merge-sort
 
 
+class ReduceOperation(Enum):
+    MAX = 1
+    MIN = 2
+    SUM = 3
+    AVG = 4
+    COUNT = 5
+
+
+class ReduceOption:
+
+    def __init__(self, column: str, agg: ReduceOperation):
+        self.column = column
+        self.agg = agg
+
+
+class Reducer:
+
+    def __init__(self, option: ReduceOption):
+        self.column = option.column
+        self.agg = option.agg
+        self.sum = 0
+        self.cnt = 0
+        self.max = - math.inf
+        self.min = math.inf
+
+    def append(self, record: dict[str: object]):
+        self.sum += record[self.column]
+        self.cnt += 1
+        self.max = max(self.max, record[self.column])
+        self.min = min(self.min, record[self.column])
+
+    def get_max(self):
+        return self.max
+
+    def get_min(self):
+        return self.min
+
+    def get_sum(self):
+        return self.sum
+
+    def get_count(self):
+        return self.cnt
+
+    def get_avg(self):
+        return self.sum / self.cnt
+
+    def reduce(self):
+        if self.agg == ReduceOperation.MAX:
+            return self.get_max()
+        elif self.agg == ReduceOperation.MIN:
+            return self.get_min()
+        elif self.agg == ReduceOperation.COUNT:
+            return self.get_count()
+        elif self.agg == ReduceOperation.SUM:
+            return self.get_sum()
+        elif self.agg == ReduceOperation.AVG:
+            return self.get_avg()
+        raise NotImplemented
+
+
 class GroupByOption:
 
-    def __init__(self, columns: list[str], new_columns: list[str], having_selector: Selector):
-        self.columns = columns
-        self.new_columns = new_columns
-        self.having_selector = having_selector
+    def __init__(self, column: str, reduce_options: list[ReduceOption]):
+        """
+        :param column: group by column
+        :param reduce_options: e.g. max(col1), avg(col2), notice: col1 and col2 should not be the same as self.column
+        """
+        self.column = column
+        self.options = reduce_options
 
 
 class TableManipulator:
@@ -117,54 +181,6 @@ class TableManipulator:
         return new_table
 
     @staticmethod
-    def _sort_and_split_on_column(src_table: Table, sort_option: SortOption) -> list[Table]:
-        """
-        split one sorted table into many tables according to the sorted column
-        all records in any one of the result table have the same value of the column
-        :param src_table:
-        :param column:
-        :return:
-        """
-        tm = get_table_manager()
-        new_table, status = tm.create_tmp_table(src_table.metadata)
-        if not status.ok():
-            raise ValueError("failed to create empty table")
-        tables: list[Table] = [new_table]
-
-        if src_table.chunk_manager.is_empty_table():
-            return tables
-
-        column = sort_option.column
-        sorted_table = TableManipulator.sort(src_table, [sort_option])
-        last_value = sorted_table.chunk_manager.get_fist_chunk()[0][0][column]
-        new_records = []
-        for chunk in sorted_table.chunk_manager.get_iter():
-            for entry in chunk:
-                if last_value != entry[column]:
-                    # dump records
-                    status = tables[-1].chunk_manager.dump_bulk(new_records)
-                    if not status.ok():
-                        raise ValueError("failed to dump bulk")
-                    new_table, status = tm.create_tmp_table(src_table.metadata)
-                    if not status.ok():
-                        raise ValueError("failed to create empty table")
-                    new_records = []
-                    last_value = entry[column]
-                    tables.append(new_table)
-                new_records.append(entry)
-                # dump new_records
-                if len(new_records) == config.max_chunk_size:
-                    status = tables[-1].chunk_manager.dump_bulk(new_records)
-                    if not status.ok():
-                        raise ValueError("failed to dump bulk")
-                    new_records = []
-        if len(new_records) != 0:
-            status = tables[-1].chunk_manager.dump_bulk(new_records)
-            if not status.ok():
-                raise ValueError("failed to dump bulk")
-        return tables
-
-    @staticmethod
     def _sort_each_chunk(src_table: Table, column: str, is_asc: bool) -> Table:
         new_table, status = get_table_manager().create_tmp_table(src_table.metadata)
         if not status.ok():
@@ -246,6 +262,12 @@ class TableManipulator:
         return input_table
 
     @staticmethod
+    def _sort_one_column(src_table, sort_options: SortOption):
+        column, is_asc, ways = sort_options.column, sort_options.is_asc, sort_options.ways
+        table = TableManipulator._sort_each_chunk(src_table, column, is_asc)
+        return TableManipulator._merge_sort(table, ways, column, is_asc)
+
+    @staticmethod
     def sort(src_table: Table, sort_options: list[SortOption]) -> Table:
         """
         sort src_table, use 100-way merge sort
@@ -262,42 +284,58 @@ class TableManipulator:
         :return: result table
         """
         # TODO implement multi-field sorting
-        column, is_asc, ways = sort_options[0].column, sort_options[0].is_asc, sort_options[0].ways
-        table = TableManipulator._sort_each_chunk(src_table, column, is_asc)
-        return TableManipulator._merge_sort(table, ways, column, is_asc)
+        return TableManipulator._sort_one_column(src_table, sort_options[0])
 
     @staticmethod
-    def rename_fields(src_table: Table, field_rename_map: dict[str, str]) -> Table:
-        for k in field_rename_map:
-            if src_table.metadata.get_field_info(k) is None:
-                raise ValueError("{} is not in table {}".format(k, src_table.name))
+    def _sort_and_split_on_column(src_table: Table, sort_option: SortOption) -> list[Table]:
+        """
+        split one sorted table into many tables according to the sorted column
+        all records in any one of the result table have the same value of the column
+        :param src_table:
+        :param column:
+        :return:
+        """
         tm = get_table_manager()
-        new_fields = []
-        for field_info in src_table.metadata.get_all_fields():
-            if field_info.name in field_rename_map:
-                new_fields.append({field_rename_map[field_info.name]: field_info.value_type})
-            else:
-                new_fields.append({field_info.name: field_info.value_type})
-        new_metadata = Metadata(src_table.metadata.table_name, src_table.metadata.db_type, new_fields)
-        new_table, status = tm.create_tmp_table(new_metadata)
+        new_table, status = tm.create_tmp_table(src_table.metadata)
         if not status.ok():
-            raise ValueError("failed to create new tmp table")
-        return new_table
+            raise ValueError("failed to create empty table")
+        tables: list[Table] = [new_table]
+
+        if src_table.chunk_manager.is_empty_table():
+            return tables
+
+        column = sort_option.column
+        sorted_table = TableManipulator.sort(src_table, [sort_option])
+        last_value = sorted_table.chunk_manager.get_fist_chunk()[0][0][column]
+        new_records = []
+        for chunk in sorted_table.chunk_manager.get_iter():
+            for entry in chunk:
+                if last_value != entry[column]:
+                    # dump records
+                    status = tables[-1].chunk_manager.dump_bulk(new_records)
+                    if not status.ok():
+                        raise ValueError("failed to dump bulk")
+                    new_table, status = tm.create_tmp_table(src_table.metadata)
+                    if not status.ok():
+                        raise ValueError("failed to create empty table")
+                    new_records = []
+                    last_value = entry[column]
+                    tables.append(new_table)
+                new_records.append(entry)
+                # dump new_records
+                if len(new_records) == config.max_chunk_size:
+                    status = tables[-1].chunk_manager.dump_bulk(new_records)
+                    if not status.ok():
+                        raise ValueError("failed to dump bulk")
+                    new_records = []
+        if len(new_records) != 0:
+            status = tables[-1].chunk_manager.dump_bulk(new_records)
+            if not status.ok():
+                raise ValueError("failed to dump bulk")
+        return tables
 
     @staticmethod
-    def group_by(src_table: Table, group_by_option: 'GroupByOption') -> Table:
-        """
-        group by table
-        :param src_table: input table, only support single table
-        :param group_by_option: options to execute groupBy operation
-        :return: result table
-        """
-        # TODO implement it
-        # no need for implementing subquery, it can simply be done by sub query)
-        # 1.
-
-    @staticmethod
-    def concat(tables: list[Table]) -> Table:
+    def _concat(tables: list[Table]) -> Table:
         """
         concat multiple table into a single table (keep the sequence)
         :param tables: all tables to be merged (order matters)
@@ -328,6 +366,104 @@ class TableManipulator:
                 raise RuntimeError("failed to dump records")
 
         return new_table
+
+    @staticmethod
+    def rename_fields(src_table: Table, field_rename_map: dict[str, str]) -> Table:
+        for k in field_rename_map:
+            if src_table.metadata.get_field_info(k) is None:
+                raise ValueError("{} is not in table {}".format(k, src_table.name))
+        tm = get_table_manager()
+        new_fields = []
+        for field_info in src_table.metadata.get_all_fields():
+            if field_info.name in field_rename_map:
+                new_fields.append({field_rename_map[field_info.name]: field_info.value_type})
+            else:
+                new_fields.append({field_info.name: field_info.value_type})
+        new_metadata = Metadata(src_table.metadata.table_name, src_table.metadata.db_type, new_fields)
+        new_table, status = tm.create_tmp_table(new_metadata)
+        if not status.ok():
+            raise ValueError("failed to create new tmp table")
+        return new_table
+
+    @staticmethod
+    def _decorate_reduced_column_name(name: str, agg: ReduceOperation):
+        return constant.REDUCED_COLUMN_NAME_SEP.join([name, agg.name])
+
+    @staticmethod
+    def _reduce_table(src_table: Table, group_by_column: str, reduce_options: list[ReduceOption]) -> Table:
+        """
+        reduce all entries in a table into a single entry
+        must call _sort_and_split_on_column first, thus all entries in this table share the same value for the group by column
+        :param src_table:
+        :param reduce_options: support aggregation functions on multiple columns (but not the group by column!)
+        :return: a table with only one entry
+        """
+        if src_table.chunk_manager.is_empty_table():
+            raise RuntimeError("can't reduce empty table")
+
+        for option in reduce_options:
+            if option.column == group_by_column:
+                raise RuntimeError("can't call reduce function on group by column: {}".format(group_by_column))
+
+        tm = get_table_manager()
+        # group_by_column should be untouched
+        # max(col) will be renamed as MAX_col
+        new_fields = [{group_by_column: src_table.metadata.get_field_type(group_by_column)}]
+        reducers = []
+        for field_info in src_table.metadata.get_all_fields():
+            for option in reduce_options:
+                if field_info.get_name() == option.column:
+                    new_field_name = TableManipulator._decorate_reduced_column_name(field_info.get_name(), option.agg)
+                    if new_field_name in [reducer.column for reducer in reducers]:
+                        raise RuntimeError("duplicated aggregated field detected: {}".format(new_field_name))
+                    reducers.append(Reducer(option))
+                    # TODO handle potential type transferring e.g. int -> float
+                    # TODO compatible with nosql
+                    new_fields.append({new_field_name: 'float'})
+        new_table, status = tm.create_tmp_table(Metadata(src_table.metadata.table_name, src_table.metadata.db_type, new_fields))
+        if not status.ok():
+            raise RuntimeError("can't create tmp table")
+
+        for chunk in src_table.chunk_manager.get_iter():
+            for entry in chunk:
+                for reducer in reducers:
+                    reducer.append(entry)
+
+        chunk, status = src_table.chunk_manager.get_fist_chunk()
+        if not status.ok():
+            raise RuntimeError("can't load src table")
+        new_record = {group_by_column: chunk[0][group_by_column]}
+        for i, field_info in enumerate(new_table.metadata.get_all_fields()[1:]):
+            new_record[field_info.name] = reducers[i].reduce()
+
+        status = new_table.chunk_manager.dump_one(new_record)
+        if not status.ok():
+            raise RuntimeError("can't dump new record")
+        return new_table
+
+    @staticmethod
+    def group_by(src_table: Table, group_by_option: 'GroupByOption') -> Table:
+        """
+        group by col can be implemented by:
+            1. gathering all fields we are interested, supported fields are: col, max_col,min_col, sum_col, count_col, avg_col
+            2. call _sort_and_split_on_column, get a bunch of tables
+            3. reduce each table to a table with a single line
+            4. merge all these reduced tables
+        This only involves O(N) I/O operations, N is entry cnt, which is acceptable
+        no need for implementing subquery, it can simply be done by sub query)
+        will rename max(col), min(col), sum(col), count(col), avg(col) to max_col, min_col, sum_col, count_col, avg_col
+
+        :param src_table: input table, only support single table
+        :param group_by_option: options to execute groupBy operation
+        :return: result table
+        """
+        # TODO support group by on multiple columns & group by joined table
+        tables = TableManipulator._sort_and_split_on_column(src_table, SortOption(group_by_option.column))
+        new_tables = []
+        for table in tables:
+            new_tables.append(TableManipulator._reduce_table(table, group_by_option.column, group_by_option.options))
+
+        return TableManipulator._concat(new_tables)
 
     @staticmethod
     def join(tables: list[Table], join_option: 'JoinOption') -> Table:
@@ -429,7 +565,7 @@ if __name__ == "__main__":
         assert all(entry['col1'] == pair[0] for entry in entries)
 
     # test concat tables
-    new_table_4 = TableManipulator.concat(tables)
+    new_table_4 = TableManipulator._concat(tables)
     sorted_random_data = sorted(random_data, key=lambda item: item['col1'])
     i = 0
     for chunk in new_table_4.chunk_manager.get_iter():
@@ -444,3 +580,61 @@ if __name__ == "__main__":
     new_table_3 = TableManipulator.rename_fields(table, {"col1": "col2", "col2": "col1"})
     assert new_table_3.metadata.get_all_fields()[0].get_name() == "col2"
     assert new_table_3.metadata.get_all_fields()[1].get_name() == "col1"
+
+    # test group by
+    table_name = "test_manipulator_groupby"
+    tm.drop_table(table_name)
+    table, status = tm.create_table(table_name, Metadata(table_name, constant.DB_TYPE_SQL, [{"col1": "int"}, {"col2": "float"}, {"col3": "str"}]))
+    assert status.ok()
+    status = table.insert({"col1": 1, "col2": 1, "col3": "a"})
+    assert status.ok()
+    status = table.insert({"col1": 1, "col2": 1, "col3": "a"})
+    assert status.ok()
+    status = table.insert({"col1": 1, "col2": 2, "col3": "a"})
+    assert status.ok()
+    status = table.insert({"col1": 1, "col2": 3, "col3": "a"})
+    assert status.ok()
+    status = table.insert({"col1": 1, "col2": 0.5, "col3": "b"})
+    assert status.ok()
+    status = table.insert({"col1": 1, "col2": 1.5, "col3": "b"})
+    assert status.ok()
+    status = table.insert({"col1": 1, "col2": 1.0, "col3": "b"})
+    assert status.ok()
+    new_table = TableManipulator.group_by(table, group_by_option=GroupByOption(
+        column="col3",
+        reduce_options=[
+            ReduceOption('col1', ReduceOperation.MAX),
+            ReduceOption('col1', ReduceOperation.MIN),
+            ReduceOption('col1', ReduceOperation.SUM),
+            ReduceOption('col1', ReduceOperation.COUNT),
+            ReduceOption('col1', ReduceOperation.AVG),
+            ReduceOption('col2', ReduceOperation.MAX),
+            ReduceOption('col2', ReduceOperation.SUM),
+            ReduceOption('col2', ReduceOperation.COUNT),
+            ReduceOption('col2', ReduceOperation.AVG),
+        ]))
+
+    # check metadata:
+    assert new_table.metadata.get_all_field_names() == ['col3', 'col1__MAX', 'col1__MIN', 'col1__SUM', 'col1__COUNT', 'col1__AVG', 'col2__MAX', 'col2__SUM', 'col2__COUNT', 'col2__AVG']
+    assert new_table.metadata.get_all_fields()[0].get_value_type() == "str"
+    assert all(field.get_value_type() == "float" for field in new_table.metadata.get_all_fields()[1:])
+
+    # check content
+    chunk, status = new_table.chunk_manager.get_fist_chunk()
+    assert status.ok()
+    assert len(chunk) == 2  # group by is in effect
+    assert chunk[0]['col1__MAX'] == 1
+    assert chunk[0]['col1__MIN'] == 1
+    assert chunk[1]['col1__COUNT'] == 3
+    assert chunk[1]['col1__AVG'] == 1
+    assert chunk[0]['col2__MAX'] == 3
+    assert 'col2__MIN' not in chunk[0]
+    assert chunk[0]['col2__SUM'] == 7
+    assert chunk[0]['col2__COUNT'] == 4
+    assert chunk[0]['col2__AVG'] == 7 / 4
+    assert chunk[1]['col2__MAX'] == 1.5
+    assert 'col2__MIN' not in chunk[1]
+    assert chunk[1]['col2__SUM'] == 3
+    assert chunk[1]['col2__COUNT'] == 3
+    assert chunk[1]['col2__AVG'] == 1
+
