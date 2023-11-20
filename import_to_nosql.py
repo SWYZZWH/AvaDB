2ee5cdcc543d12bad48938a4479d3879faa89455
zwh <843532380@qq.com>
2023-11-20 12:04:36 -0800

    fix import_nosql_data script

--- import_to_nosql.py
+++ import_to_nosql.py
@@ -13,28 +13,6 @@ import tempfile
 logger = logger.get_logger(constant.CLI_NAME)
 
 
-def download_file(url, query) -> str:
-    try:
-        logger.info("Try to download from url {} ...".format(url))
-        with requests.get(url, json=query, stream=True) as r:
-            logger.info("Receive response from url {}, status code: {}".format(url, r.status_code))
-            r.raise_for_status()
-
-            temp_file = tempfile.NamedTemporaryFile(mode='w+t', delete=False)
-            logger.info("Tmp file {} is created to save file from {}".format(temp_file.name, url))
-            for chunk in r.iter_content(chunk_size=constant.DOWNLOAD_CHUNK_SIZE):
-                temp_file.write(chunk.decode('utf-8'))
-                temp_file.flush()
-
-            logger.info("Successfully download file from {} and save to {}".format(url, temp_file.name))
-            temp_file.close()
-            return temp_file.name
-    except Exception as e:
-        logger.error("Download file from {} failed, due to: {}".format(url, e))
-
-    return ""
-
-
 def print_file(file_name: str):
     with open(file_name, 'r', encoding='utf-8') as file:
         line = file.readline()
@@ -97,22 +75,24 @@ def send_bulk_insert_commands(commands):
                 print("Failed to execute command:", response.text)
         except Exception as e:
             logger.error(f"Error sending command to {full_url}: {e}")
+
+
 # CSV 文件路径
 
 def test(commands):
-    base_url = "http://localhost:9527/insert"
-    try:
+    base_url = "http://localhost:9527"
+    drop_url = "/".join([base_url, "drop"])
+    create_url = "/".join([base_url, "create"])
+    insert_url = "/".join([base_url, "insert"])
 
-        encoded_json = urllib.parse.quote(json.dumps(commands))
-        full_url = f"{base_url}"
-        response = requests.get(full_url, json=encoded_json)
+    requests.get(drop_url, json=json.dumps({"table_name": "allsales"}))
+    requests.get(create_url, json=json.dumps({"table_name": "allsales"}))
+    response = requests.get(insert_url, json=json.dumps(commands))
+    if response.status_code == 200:
+        print("Command executed successfully:", response.text)
+    else:
+        print("Failed to execute command:", response.text)
 
-        if response.status_code == 200:
-            print("Command executed successfully:", response.text)
-        else:
-            print("Failed to execute command:", response.text)
-    except Exception as e:
-        logger.error(f"Error sending command to {full_url}: {e}")
 
 csv_file_path = 'dataset/allSales.csv'
 
@@ -126,8 +106,10 @@ def read_csv_to_dict(csv_file_path):
             data.append(row)
     return data
 
+
 # 从 CSV 读取数据
 csv_data = read_csv_to_dict(csv_file_path)
+
 
 # 将 Python 字典列表转换为目标 JSON 格式
 def convert_data(data):
@@ -146,21 +128,21 @@ def convert_data(data):
         }
     }
 
+
 # 执行转换
 converted_data = convert_data(csv_data)
+
 
 def generate_nosql_commands(json_data):
     commands = []
     for record in json_data['insert']['records']:
         command = {
-            "type": "nosql",
-            "insert": {
-                "table_name": json_data['insert']['table_name'],
-                "records": [record]
-            }
+            "table_name": json_data['insert']['table_name'],
+            "records": [record]
         }
         commands.append(command)
     return commands
+
 
 # 生成命令
 commands = generate_nosql_commands(converted_data)
@@ -179,4 +161,3 @@ test(cmd)
 #
 # if __name__ == "__main__":
 #     main()
-
