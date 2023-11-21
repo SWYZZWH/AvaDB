ed98e1aa9478d8ff6cd6217a6a14df34604b7889
zwh <843532380@qq.com>
2023-11-20 18:55:10 -0800

    add scripts to import data

--- scripts/import_sql_data.sh
+++ scripts/import_sql_data.sh
@@ -6,4 +6,6 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 # Change to the parent directory of the script
 cd "$SCRIPT_DIR"/..
 
-python3 import_data.py dataset/airline_delay.json
+python3 import_data.py dataset/allSales.csv
+python3 import_data.py dataset/xboxSales.csv
+python3 import_data.py dataset/PS4Sales.csv
